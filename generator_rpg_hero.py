36a6bd5bf935a1df67fefb9e29d830284c37278b
Asadbek <alogcoden@gmail.com>
2026-06-02 13:23:58 +0500

    Add files via upload

---
+++ generator_rpg_hero.py
@@ -0,0 +1,14 @@
+print("Welcome to the RPG Character Generator!\n")
+name_hero = input("Enter the character's name: ")
+city = input("Enter a city: ")
+class_hero = input("Enter the class: ")
+
+print("="*20)
+print("CHARACTER SHEET")
+print("="*20)
+
+print("Name: " + name_hero)
+print("City: " + city)
+print("Class: " + class_hero)
+
+print("\nLength of the character's name:",len(name_hero),"characters")
